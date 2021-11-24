d1de26f8db9825b3de880e673f95597f49bf7570
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2021-11-24 14:30:17 +0100

    Create guid cookie in IDP.

--- src/IDP/Controllers/MVC/AuthController.cs
+++ src/IDP/Controllers/MVC/AuthController.cs
@@ -6,6 +6,9 @@ using IDP.ViewModels.Auth;
 using IdentityServer4.Services;
 using Serilog;
 using Common;
+using System;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
 
 namespace IDP.Controllers.MVC
 {
@@ -26,10 +29,6 @@ namespace IDP.Controllers.MVC
         [HttpGet("Login")]
         public IActionResult Login(string returnUrl)
         {
-            //Log.Information("Hello, from AuthController.cs in IDP");
-
-            
-
             return View("Login", new LoginViewModel { ReturnUrl = returnUrl ?? uri.IDP });
         }
 
@@ -46,10 +45,34 @@ namespace IDP.Controllers.MVC
 
             if (result.Succeeded)
             {
-                var a = 12;
+                //Solution-big-picture strategy:
+                //A: Client login to IDP-microservice, and IDP create a cookie with a GUID. The guid is from CoreIdentity user management.
+                //B: Client connect to UI-head(MVC) with access-token.If endpoint is reached and token has proper access, mvc read the cookie, and use the guid as a key in a sessionStorage. The value in the sessionStorage is a Basket.
+                //C: Session storage is global, and every ViewModel contain a Basket through inheritance.
+
+                //Conclusion: here we write a cookie to the client with the GUID. This is no security risk, becouse the accesstoken protect all endpoints
+                //And the guid is only used to link the basket to the client.
+                //CreateCookie("h-pax.cookie", "myGuid");
+
+                var appUser = _signInManager.UserManager.Users.SingleOrDefault(r => r.UserName == vm.Username);
+                CreateCookie("h-pax.cookie", appUser.Id.ToString());
+
                 return Redirect(vm.ReturnUrl ?? uri.IDP);
             }
             return View("Login", new LoginViewModel { ReturnUrl = vm.ReturnUrl ?? uri.IDP });
+        }
+
+        public void CreateCookie(string key, string value)
+        {
+            CookieOptions option = new CookieOptions()
+            {
+                Path = "/",
+                HttpOnly = false, //True = prevent client-side JS from accessing the cookie vlaue
+                Secure = false, //True = only HTTPS
+                Expires = DateTime.Now.AddMinutes(10)
+            };
+
+            Response.Cookies.Append(key, value, option);
         }
     }
 }
