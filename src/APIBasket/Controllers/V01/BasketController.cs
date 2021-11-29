62847d9fce66ac4a61dcda4522bbdc3c67ab4abb
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2021-11-29 15:49:24 +0100

    APIBasket template code added

---
+++ src/APIBasket/Controllers/V01/BasketController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using APIBasket.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace APIBasket.Controllers.V01
+{
+    [Route("api/V01/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class BasketController : ControllerBase
+    {
+
+        //private readonly IProductCatalogRepository _IproductCatalogRepository;
+        private readonly IMapper _mapper;
+
+        public BasketController(IMapper mapper)
+        {
+            //_IproductCatalogRepository = productCatalogRepository;
+            _mapper = mapper;
+        }
+
+        [HttpPost("GetBasket")]
+        public ActionResult<Basket> GetBasket(Guid UserGuid)
+        {
+
+            //List<Product> result = _IproductCatalogRepository.ReadAllProducts();
+            //return Ok(_mapper.Map<List<Models.ProductDto>>(result));
+
+            return new Basket();
+        }
+    }
+}
+
---
+++ src/APIBasket/Entities/Basket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBasket.Entities
+{
+    public class Basket
+    {
+        public Guid BasketId { get; set; }
+        public Guid UserId { get; set; }
+        public List<BasketLine> BasketLines { get; set; }
+        public Guid? CouponId { get; set; }
+    }
+}
